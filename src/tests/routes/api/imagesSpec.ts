4623783b719f6138dbdbffcf01f52f691816f492
Daniele Visaggio <visaggio.daniele@gmail.com>
2021-12-19 00:07:47 +0100

    test: added a before all condition to be met before running the 'actual photo resize test', i.e. remove files from asset/thumb

--- src/tests/routes/api/imagesSpec.ts
+++ src/tests/routes/api/imagesSpec.ts
@@ -1,21 +1,30 @@
 import * as path from 'path';
+import { promises as fs } from 'fs';
 import sharp from 'sharp';
-import { openPhoto } from '../../../utils/util';
+import { openPhoto, listPhotos } from '../../../utils/util';
 import { resizeAndSavePhoto } from '../../../routes/api/images';
 
 describe('test actual photo resize', () => {
   const fullPhotoPath = path.join(process.cwd(), 'asset', 'full');
   const thumbPhotoPath = path.join(process.cwd(), 'asset', 'thumb');
-  const filename = 'tux';
-  // random values
+  // remove any existing resized photo before running test
+  beforeAll(async () => {
+    const photos = await listPhotos(thumbPhotoPath);
+    for (const photo of photos) {
+      await fs.unlink(path.join(thumbPhotoPath, photo));
+    }
+  });
+
+  // random resize values
   const width = 122;
   const height = 237;
   const resizedPicName = `${filename}_W${width}_H${height}`;
+  const filename = 'tux';
 
   it('should resize the given photoBuffer and save the resized image inside asset/thumb with the correct dimensions', async () => {
     // open the photo and find its extension
     const [photoBuffer, extension] = await openPhoto(filename, fullPhotoPath);
-    // resize and save
+    // resize and save <--- we are testing this step
     await resizeAndSavePhoto(
       photoBuffer,
       width,
