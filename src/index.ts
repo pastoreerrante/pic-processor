4d87a3dafe8e9ad4ec79038b2a18b6d5cf791657
Daniele Visaggio <visaggio.daniele@gmail.com>
2021-12-20 11:19:18 +0100

    refactor: moved fullPhotoPath and thumbPhotoPath constant in index.ts to make them available project-wide. Avoid recalculating them in every other modules

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as path from 'path';
 import express from 'express';
 import { Request, Response } from 'express';
 import routes from './routes/index';
@@ -5,6 +6,11 @@ import routes from './routes/index';
 const app = express();
 // http listening port
 const port = 3000;
+// path containing original photos. process.cwd() gives back
+// the project root path
+const fullPhotoPath = path.join(process.cwd(), 'asset', 'full');
+// path containing resized photos
+const thumbPhotoPath = path.join(process.cwd(), 'asset', 'thumb');
 
 // use ejs to render html template
 app.set('view engine', 'ejs');
@@ -24,4 +30,4 @@ app.listen(port, '0.0.0.0', () => {
   console.log(`server started at localhost:${port}`);
 });
 
-export default app;
+export { app as default, fullPhotoPath, thumbPhotoPath };
